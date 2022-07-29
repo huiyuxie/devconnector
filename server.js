bba437c03de9f4bb5313a13d7aee3102a392e673
huiyuxie <xhy19127@gmail.com>
2022-07-29 14:01:36 +0800

    connect mongodb

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 5000;
+const connectDB = require('./config/db');
 
+// Connect database
+connectDB();
+
+const app = express();
 app.get('/', (req, res) => res.send('API running'));
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
