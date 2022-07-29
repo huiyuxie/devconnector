1c64f3f571a0beadf76ad4c18d20327a2118ec68
huiyuxie <xhy19127@gmail.com>
2022-07-29 14:43:19 +0800

    edit routes

--- server.js
+++ server.js
@@ -6,5 +6,13 @@ connectDB();
 
 const app = express();
 app.get('/', (req, res) => res.send('API running'));
+
+// Define routes
+app.use('/api/user', require('./routes/api/user'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/post', require('./routes/api/post'));
+app.use('/api/profile', require('./routes/api/profile'));
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
