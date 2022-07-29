2d29ad01a2810be24b69c88a80b5b07a13b7efb3
huiyuxie <xhy19127@gmail.com>
2022-07-30 00:58:22 +0800

    add web token

--- routes/api/user.js
+++ routes/api/user.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User')
@@ -61,7 +62,15 @@ async (req, res) => {
                 id: user.id
             }
         }
-        jwt.sign(payload, )
+        jwt.sign(
+            payload, 
+            config.get('jwtSecret'),
+            { expireIn: 36000 },
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+            }
+        );
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server error');
