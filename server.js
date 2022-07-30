c967c75886bf1995bfbc74febeec4d29034aa8db
huiyuxie <xhy19127@gmail.com>
2022-07-30 12:54:03 +0800

    authorize

--- server.js
+++ server.js
@@ -18,6 +18,5 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/post', require('./routes/api/post'));
 app.use('/api/profile', require('./routes/api/profile'));
 
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
